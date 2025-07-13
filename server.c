62fd7efaa434cbd33bca50c23e298341c0850465
monamoon7 <mona.shaghafi@gmail.com>
2025-07-13 16:41:41 +0200

    first server

--- server.c
+++ server.c
@@ -3,16 +3,17 @@
 /*                                                        ::::::::            */
 /*   server.c                                           :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: mona <mona@student.codam.nl>                 +#+                     */
+/*   By: mshaghaf <mshaghaf@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/07/11 16:39:44 by mona          #+#    #+#                 */
-/*   Updated: 2025/07/11 16:51:10 by mona          ########   odam.nl         */
+/*   Updated: 2025/07/13 16:39:05 by mona          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
+#include "libft.h"
 
 void	signal_handler(int signal)
 {
@@ -20,4 +21,14 @@ void	signal_handler(int signal)
 		write(1, "0", 1);
 	else if (signal == SIGUSR2)
 		write(1, "1", 1);
+}
+
+int	main(void)
+{
+	struct	sigaction	sa;
+	pid_t				pid;
+	
+	pid = getpid();
+	write(1, "Server PID: ", 13);
+	ft_putnbr_fd()
 }
